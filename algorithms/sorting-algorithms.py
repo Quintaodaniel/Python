d938adbf3aa943c2a814638413133869b65d9822
Daniel Quintão <danielqferes@gmail.com>
2025-06-27 17:48:43 -0300

    Started making the sorting algorithms

---
+++ algorithms/sorting-algorithms.py
@@ -0,0 +1,18 @@
+def bubbleSort(list):
+    for i in range(len(list) - 1):
+        for j in range(len(list) - 1 - i):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    return list
+
+def selectionSort(lista):
+    n = len(lista)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if lista[j] < lista[min_index]:
+                min_index = j
+        lista[i], lista[min_index] = lista[min_index], lista[i]
+    return lista
+
+#list = [4, 2, 3, 5, 9, 1, 22, 6]
